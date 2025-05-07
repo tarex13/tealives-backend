ada5351cfb655c4acb44c9c7ce872907042ffe40
Tara Onayoade <tarex2006@gmail.com>
2025-05-07 01:31:02 -0500

    Fixing Requirements

--- tealives/settings.py
+++ tealives/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aimv!0jr)jj(ksw=#^w6x^b%+bk&&(7*34&0!tk_#kb9sfs%_q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
